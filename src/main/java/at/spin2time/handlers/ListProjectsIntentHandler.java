9a4ff885c58576387f591abe5fb0dfdcd58aa3a5
mil17586 <mil17586@spengergasse.at>
2019-11-29 10:24:53 +0100

    Small changes on ListProjectsIntentHandler

--- src/main/java/at/spin2time/handlers/ListProjectsIntentHandler.java
+++ src/main/java/at/spin2time/handlers/ListProjectsIntentHandler.java
@@ -25,6 +25,8 @@ public class ListProjectsIntentHandler implements IntentRequestHandler {
 
         Intent intent = intentRequest.getIntent();
 
+
+
         String username = intent.getSlots().get("name").getValue();
 
         String speechtext = getProjects(username);
